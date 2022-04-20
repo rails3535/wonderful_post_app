750bd92bd3fd4bc2d8567ef583d9bd196ca3dd33
YUTA UEHARA <mame5124@gmail.com>
2022-04-21 01:33:10 +0900

    ## 記事の作成、編集、削除のリファクタリング
    - 記事の作成、編集、削除をログインユーザーに紐付け

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  skip_before_action :authenticate_user!, only: %i[ index show ]
   before_action :set_article, only: %i[ show edit update destroy ]
 
   # GET /articles or /articles.json
@@ -8,11 +9,14 @@ class ArticlesController < ApplicationController
 
   # GET /articles/1 or /articles/1.json
   def show
+    @article = Article.find(params[:id])
   end
 
   # GET /articles/new
   def new
     @article = Article.new
+
+    # @article= current_user.articles.build
   end
 
   # GET /articles/1/edit
@@ -21,46 +25,35 @@ class ArticlesController < ApplicationController
 
   # POST /articles or /articles.json
   def create
-    @article = Article.new(article_params)
+      @article = current_user.articles.new(article_params)
 
-    respond_to do |format|
+
       if @article.save
-        format.html { redirect_to article_url(@article), notice: "記事が作成されました" }
-        format.json { render :show, status: :created, location: @article }
+        redirect_to @article, notice: "新しい記事を投稿しました。"
       else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @article.errors, status: :unprocessable_entity }
+        render :new, status: :unprocessable_entity
       end
-    end
   end
 
   # PATCH/PUT /articles/1 or /articles/1.json
   def update
-    respond_to do |format|
       if @article.update(article_params)
-        format.html { redirect_to article_url(@article), notice: "記事が更新されました" }
-        format.json { render :show, status: :ok, location: @article }
+        redirect_to @article, notice: "記事を更新しました。"
       else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @article.errors, status: :unprocessable_entity }
+        render :edit, status: :unprocessable_entity
       end
-    end
   end
 
   # DELETE /articles/1 or /articles/1.json
   def destroy
     @article.destroy
-
-    respond_to do |format|
-      format.html { redirect_to articles_url, notice: "記事が削除されました" }
-      format.json { head :no_content }
-    end
+    redirect_to articles_url, notice: "記事を削除しました。"
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_article
-      @article = Article.find(params[:id])
+      @article = current_user.articles.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
