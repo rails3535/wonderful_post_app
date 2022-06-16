4a546803a0818abfe7566191cc80fa2650cfe018
YUTA UEHARA <mame5124@gmail.com>
2022-06-17 02:16:01 +0900

    ## seedファイルの作成 - sample_articleのmigrationファイルを削除 - ユーザーが3つ作られる。 - メールアドレス:　 ''user001@test.com" -「001」の部分は、2人目は「002」、3人目は「003」 - パスワード:　 "test1234" - 各ユーザーに紐づいた記事がそれぞれ50個ずつ作られる - タイトル:　No.1: user001の記事 - 本文:　No.1: user001の記事の本文 - seedファイルを複数回実行しても、同じ内容のデータが出来ないように制御

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+3.times do |n|
+  user = User.find_or_create_by!(name: "#{n + 1}",email: "user00#{n + 1}@test.com",encrypted_password: "test1234") do |user|
+    user.email = "user00#{n + 1}@test.com"
+    user.encrypted_password = "test1234"
+    user.id = "#{n + 1}"
+  end
+  50.times do |i|
+    user.articles.find_or_create_by!(title:"No.#{i+1}:user00#{n + 1}の記事",content:"No.#{i+1}:user00#{n + 1}の記事の本文") do |article|
+      article.title = "No.#{i+1}:user00#{n + 1}の記事"
+      article.content = "No.#{i+1}:user00#{n + 1}の記事の本文"
+      article.user_id = "#{n + 1}"
+    end
+   end
+end
