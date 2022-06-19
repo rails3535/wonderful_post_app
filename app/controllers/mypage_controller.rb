7d072e7a2a00c2492bfacff432dd70392b726ba8
YUTA UEHARA <mame5124@gmail.com>
2022-06-19 22:52:18 +0900

    ##　マイページのページネーション追加

--- app/controllers/mypage_controller.rb
+++ app/controllers/mypage_controller.rb
@@ -1,5 +1,7 @@
 class MypageController < ApplicationController
   def show
     @articles = current_user.articles
+    @articles = Article.page(params[:page]).per(25)
   end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,11 @@
 
 3.times do |n|
-  user = User.find_or_create_by!(name: "#{n + 1}",email: "user00#{n + 1}@test.com",encrypted_password: "test1234") do |user|
-    user.email = "user00#{n + 1}@test.com"
-    user.encrypted_password = "test1234"
-    user.id = "#{n + 1}"
+  user = User.find_or_create_by!(email:"user_00#{n + 1}@test.com") do |user|
+    user.password = "test1234"
   end
   50.times do |i|
-    user.articles.find_or_create_by!(title:"No.#{i+1}:user00#{n + 1}の記事",content:"No.#{i+1}:user00#{n + 1}の記事の本文") do |article|
-      article.title = "No.#{i+1}:user00#{n + 1}の記事"
+    user.articles.find_or_create_by!(title:"No.#{i+1}:user00#{n + 1}の記事") do |article|
       article.content = "No.#{i+1}:user00#{n + 1}の記事の本文"
-      article.user_id = "#{n + 1}"
     end
-   end
+  end
 end
