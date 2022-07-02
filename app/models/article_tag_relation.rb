3d1f6bf1910d445f07f0c26abec52a5a120e22b0
YUTA UEHARA <mame5124@gmail.com>
2022-07-02 23:21:45 +0900

    ##  記事とタグの紐づけての作成と表示 - articleモデルとtagモデルを中間テーブルで紐付け - seedファイルにタグの名前を追加

--- app/models/article_tag_relation.rb
+++ app/models/article_tag_relation.rb
@@ -1,4 +1,6 @@
 class ArticleTagRelation < ApplicationRecord
   belongs_to :article
   belongs_to :tag
+  validates :article_id, presence: true
+  validates :tag_id, presence: true
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,5 +1,5 @@
 class Tag < ApplicationRecord
   validates :name, presence: true
   has_many :article_tag_relations, dependent: :destroy
-  has_many :articles, through: :article_tag_relations, dependent: :destroy
+  has_many :articles, through: :article_tag_relations
 end
